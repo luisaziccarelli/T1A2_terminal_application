1fe803690839b8e86206e8bb028be51b725fe210
LuisaZ <luisaziccarelli@gmail.com>
2019-11-02 16:37:38 +1100

    initial commit

--- funfactfile.rb
+++ funfactfile.rb
@@ -1,12 +1,15 @@
 #fun fact generator
 
+require 'faker'
 
 
-def greeting
-    puts "Hey there, would you like to learn a fun fact today?"
-    user_input = gets.chomp.to_s
-    while user_input == yes 
-        puts  
-    else "No worries! See ya next time!"
-    end
-end
+puts "Hey there, would you like to learn a fun fact today?"
+   user_input = gets.chomp.to_s
+def funfact
+options = ["yes", "no"]
+    if user_input == yes
+    puts Faker::TvShows::Friends.character #=> "Rachel Green"
+    else 
+    puts "no worries"
+end 
+end 
