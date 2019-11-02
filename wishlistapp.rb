8d8e4b037d03b1c4d3734727588f3f9b19ffcb3a
LuisaZ <luisaziccarelli@gmail.com>
2019-11-02 15:28:25 +1100

    first commit

--- wishlistapp.rb
+++ wishlistapp.rb
@@ -1,14 +1,23 @@
 # Wishlist app 
 
-puts "What would you like to add to your shopping list?"
+wishlist = 0 
 
-user_input = gets.chomp.to_s
+products = [
+    "shoes" , "socks" , "dress" , "shirts" 
+]
 
-while user_input < 0 
+puts "What products would you like to add to your shopping list?"
+
+costumer_pick = gets.chomp.to_s
+
+while 
     puts "Your item has been added! What else would you like add?"
+    wishlist += 1  
 end 
 
-
+#def basket(product1, product2)
+#list of products
+#wishlist adds products
 #a loop to keep the app running that simply asks for user input
 #a method with a parameter that adds the user input to an array
 #a method to display the contents of an array
