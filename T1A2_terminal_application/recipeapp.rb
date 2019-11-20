e6baecf6767c47f67d1bbc96b05214ff4e8c6a66
LuisaZ <luisaziccarelli@gmail.com>
2019-11-20 15:54:42 +1100

    22nd commit

--- T1A2_terminal_application/recipeapp.rb
+++ T1A2_terminal_application/recipeapp.rb
@@ -9,12 +9,6 @@ puts a.asciify("My Recipe App").colorize(:light_blue)
 
 
 
-#List of ingredients 
-#food = [ "bread", "milk" , "cheese" , "apples" , "carrot" , "steak" , "pasta" ] 
-#puts "You have bread, milk, cheese, apples, carrot, steak, pasta in your kitchen"
-# array of food in recipes
-
-
 #Welcome message
 # #def greeting(name) 
 # name = ARGV 
@@ -28,29 +22,44 @@ puts a.asciify("My Recipe App").colorize(:light_blue)
 #  puts greeting(ARGV)
 puts "Welcome to My recipe App\nThis program gives you recipes you can make with food you have available in your kitchen. Let's get started!"
 
-
 #Main ingredients
-puts "What food do you have available in your kitchen? List your main ingredients:"
-main_ingredients = gets.chomp.to_s 
+puts "Here's a list of food items typically found in most people's kitchen. \n bread, milk , cheese , apples , carrots , steak , pasta.\n Type: \n 0= yes, you have these items and wish to continue, \n 1= no, you don't have these items but wish to continue, \n 2= to exit the program"
+main_ingredients = gets.chomp.to_i
+
+case main_ingredients
+when 0
+    puts "Great, you already have some ingredients. Let's continue"
+when 1
+    puts "So you may not have the ingredients, but would like to continue anyways. Let's proceed"
+when 2
+    puts "You wished to exit the program. See you next time!"
+when 3...9
+    puts "Invalid option. Try again"
+end
+
+begin 
+    main_ingredients = gets.chomp.to_i
+end while main_ingredients == 2 
+   
 
 # Check if main ingredients are in ingredients in recipe
 
-def ingredientsmatch(main_ingredients)
+# #def ingredientsmatch(main_ingredients)
 
-ingredients_in_recipes = [ "bread", "milk" , "cheese" , "apples" , "carrot" , "steak" , "pasta" ] 
+# ingredients_in_recipes = [ "bread", "milk" , "cheese" , "apples" , "carrots" , "steak" , "pasta" ] 
 
-i = 0
-    for i in ingredients_in_recipes
-         if ingredients_in_recipes.include?(main_ingredients)
-             puts "Great, we might have some recipes to match your food"
-         else
-             puts "Sorry, I am afraid we do not have recipes available that match your food"
-         end
-         break
-     end  
- end
+# i = 0
+#      for i in ingredients_in_recipes
+#          if ingredients_in_recipes.include?(main_ingredients)
+#              puts "Great, we might have some recipes to match your food"
+#          else  
+#              puts "Sorry, I am afraid we do not have recipes available that match your food"
+#          end
+#          break 
+#      end
+# end  
 
- puts ingredientsmatch(main_ingredients)
+# puts ingredientsmatch(main_ingredients)
 
 #Dietary restrictions
 
@@ -60,7 +69,7 @@ vegan = gets.chomp.to_s
 puts "Are you gluten free? Type y for yes or n for no"
 gf = gets.chomp.to_s
 
-#START METHOD 1 
+#START METHOD 1- vegan or gf
 
 def veganorgf (vegan,gf)
 
@@ -83,7 +92,7 @@ rescue TypeError
     puts "TypeError handled"
 end 
 
-   
+#Combinations of dietary restrictions    
 def diet_combinations(vegan, gf)
 
     if (vegan == "y") && (gf == "y")
@@ -102,7 +111,8 @@ end
 diet_combinations(vegan, gf)
 
 
-# START METHOD 2
+# START METHOD 2: Selecting recipes 
+
  def choiceofrecipe(vegan, gf)   
 
     recipes = [
